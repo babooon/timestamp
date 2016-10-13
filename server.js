d8578cbc036e85768c30f2f3b1d63ba9d1d6ce2d
babooon <petramueller30@gmx.de>
2016-10-13 15:29:53 +0000

    it works

--- server.js
+++ server.js
@@ -1,13 +1,35 @@
-var express = require('express');
-var app = express();
+var http = require('http');
+var url = require('url');
 
+var server = http.createServer(function (req, res) {
+    
+    var unix, natural;
+    var input = decodeURI(req.url).substring(1);
+    
+    if (input == ""){
+        res.end("Please pass a string that contains either a unix timestamp or a natural language date!");
+    }
+    
+    if (!isNaN(parseInt(input)))
+        var date = new Date(parseInt(input)*1000);
+    else
+        var date = new Date(input);
 
-app.use(express.static("public"));
+    
+    if (isNaN(date.valueOf())){
+        natural = null;
+        unix = date.valueOf();
+    }else{
+        natural = date.toDateString();
+        unix = date.valueOf()/1000;
+    }
 
-/*app.get('/', function (req, res) {
-  res.send('Hello World!');
-});*/
-
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    
+    res.end(JSON.stringify({ 
+        unix: unix,
+        natural: natural
+    }));
 });
+
+server.listen(8080);
